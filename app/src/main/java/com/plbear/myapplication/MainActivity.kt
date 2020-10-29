c4404e11faf5ae01910412ea9ec1aac84151b482
闫永俊 <yanyojun@foxmail.com>
2020-10-29 22:13:05 +0800

    sdasd

--- app/src/main/java/com/plbear/myapplication/MainActivity.kt
+++ app/src/main/java/com/plbear/myapplication/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.plbear.myapplication
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.plbear.myapplication.databinding.ActivityMainBinding
@@ -17,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         binding.etBase.setText(baseNum.toString())
         val point = sp.getFloat("point", 27f)
         binding.etPoint.setText(point.toString())
-        binding.tvDesc.text = sp.getString("last_text","")
+        binding.tvDesc.text = sp.getString("last_text", "")
 
         binding.btnCal.setOnClickListener {
             val base = binding.etBase.text.toString().toInt()
@@ -31,24 +30,27 @@ class MainActivity : AppCompatActivity() {
 
     private fun text(base: Int, tmp: Float): String {
         val cnt = 5
-        val point = 100 - tmp
-        Log.e("yanlog","point $point")
-        val scale = if (point >= 95) 60f
-        else if (point >= 90) 40f
-        else if (point >= 80) 15f
-        else if (point >= 70) 8f
-        else if (point >= 60) 5f
-        else if (point >= 50) 3f
-        else if (point >= 40) 1.5f
-        else if (point >= 30) 0.8f
-        else if (point >= 20) 0.3f
-        else if (point >= 15) -0.5f
-        else if (point >= 10) -5f
-        else if (point >= 5) -30f
-        else -100f
+        val point = tmp
+        val scale = if (point >= 95) 140f
+        else if (point >= 90) 100f
+        else if (point >= 80) 50f
+        else if (point >= 70) 20f
+        else if (point >= 60) 7f
+        else if (point >= 50) 5f
+        else if (point >= 40) 4f
+        else if (point >= 30) 3f
+        else if (point >= 20) 2f
+        else if (point >= 15) 1f
+        else if (point >= 10) 0.5f
+        else if (point >= 5) 0.3f
+        else if (point >= 4) -80f
+        else if (point >= 3) -90f
+        else if (point >= 2) -100f
+        else if (point >= 1) -200f
+        else -400f
 
         if (scale > 0) {
-            val echoCnt = (base * scale / (20 * 5) / cnt).toInt()
+            val echoCnt = (base * scale / 20).toInt()
             return "每天需要定投: $echoCnt"
         }
         val echoCnt = (base * scale / 20).toInt()
